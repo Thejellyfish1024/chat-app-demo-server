cdc23b52b59e7592d8d756ddfb3c7da73816faa6
Thejellyfish1024 <alamin102410@gmail.com>
2024-03-27 08:46:37 +0600

    implemented mongodb and socket io for real time messeging

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
+const port = process.env.port || 3000
 
+// 
 app.use(cors());
+app.use(express.json());
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
+const server = http?.createServer(app)?.listen(port)
+
 
 const io = new Server(server, {
   cors: {
@@ -14,6 +20,8 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
+
+// console.log("io", io);
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -27,6 +35,60 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING in 3001");
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s79pxyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+const database = client.db('chatDemo');
+const chatCollection = database.collection('chats');
+
+
+
+app.get('/all-chats', async (req, res) => {
+  const result = await chatCollection.find().toArray()
+  res.send(result)
+})
+
+app.post('/new-chat', async (req, res) => {
+  const newChat = req?.body;
+  console.log("hitting message", newChat);
+  const result = await chatCollection?.insertOne(newChat);
+  res.send(result)
+})
+
+
+
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// server.listen(3001, () => {
+//   console.log("SERVER IS RUNNING in 3001");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
